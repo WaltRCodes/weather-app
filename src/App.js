34705cb8e288ac257b5652999d342cc04a49039f
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-03 19:36:18 -0400

    added api key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import {env} from './components/env';
 import './App.css';
 
 export default class App extends Component {
@@ -8,7 +8,13 @@ export default class App extends Component {
 
   }
   componentDidMount() {
-
+    fetch("http://api.openweathermap.org/data/2.5/forecast?id=5128638&appid="+env.key)
+      .then(response => response.json()) 
+      .then(
+        result => {
+          console.log(result);
+        }
+      ).catch(e => console.log("there's a error", e))
   }
   
   render (){
