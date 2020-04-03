bb27212c02185e04aedcc4e09701cf768af166a0
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-03 19:12:28 -0400

    created componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class App extends Component {
+  
+  componentDidMount() {
+    
+  }
+  
   render (){
+    
     return (
       <div className="App">
         
