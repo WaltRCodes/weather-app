c305b5f79b85b8790e3429757e915ea2a0c528d2
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-06 20:34:11 -0400

    added parenthesis

--- src/components/Monday.js
+++ src/components/Monday.js
@@ -3,7 +3,7 @@ import {useLocation, useParams} from "react-router-dom";
 function Monday() {
     var params = new URLSearchParams(useLocation().search);
     var date = params.get("date");
-return <p>Monday {date}</p>;
+return (<p>Monday {date}</p>);
   }
 
 export default Monday;
