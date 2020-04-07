be18161d1e72f92983a0bb4b2101b23029b39770
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-06 22:38:08 -0400

    updated Tuesday.js

--- src/components/Tuesday.js
+++ src/components/Tuesday.js
@@ -1,1 +1,5 @@
 import React from 'react';
+import cloudy from './images/cloudy.png';
+import rainy from './images/rainy.png';
+import sunny from './images/sunny.png';
+import snowy from './images/snowy.png';
