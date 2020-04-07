b46848f7fb9837dd2f99a15aa00279373b60b965
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-06 22:11:02 -0400

    added min

--- src/components/Monday.js
+++ src/components/Monday.js
@@ -5,12 +5,14 @@ function Monday() {
     let date = params.get("date");
     let description = params.get("description");
     let current = params.get("current");
+    let min = params.get("min");
 return (
     <div>
         <p>Monday</p>
         <p>{date}</p>
         <p>{description}</p>
         <p>{current}</p>
+        <p>{min}</p>
     </div>
 
 );
