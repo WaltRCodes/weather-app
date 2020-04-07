ef5d59d809695061c8f99b09e839218791971c41
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-06 22:49:43 -0400

    created Friday.js

---
+++ src/components/Friday.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import cloudy from './images/cloudy.png';
+import rainy from './images/rainy.png';
+import sunny from './images/sunny.png';
+import snowy from './images/snowy.png';
+import {useLocation} from "react-router-dom";
