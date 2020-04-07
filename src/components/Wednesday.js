8abac155f4ef76dfdb386b4f52d2e6e0529c0639
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-06 22:44:08 -0400

    updated Wednesday.js

--- src/components/Wednesday.js
+++ src/components/Wednesday.js
@@ -4,3 +4,14 @@ import rainy from './images/rainy.png';
 import sunny from './images/sunny.png';
 import snowy from './images/snowy.png';
 import {useLocation} from "react-router-dom";
+
+function Wednesday() {
+return (
+    <div>
+        <p>Wednesday</p>
+    </div>
+
+);
+  }
+
+export default Wednesday;
