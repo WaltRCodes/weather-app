732ddbfee6728ba4067bd5e3d491a235d9fcdb96
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-06 22:38:33 -0400

    updated Tuesday.js

--- src/components/Tuesday.js
+++ src/components/Tuesday.js
@@ -3,3 +3,4 @@ import cloudy from './images/cloudy.png';
 import rainy from './images/rainy.png';
 import sunny from './images/sunny.png';
 import snowy from './images/snowy.png';
+import {useLocation} from "react-router-dom";
