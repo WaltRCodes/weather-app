0e46506ac3bf052f9828fae8106da7ce5bc0d74f
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-06 22:32:55 -0400

    added more conditionals

--- src/components/Monday.js
+++ src/components/Monday.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {useLocation, useParams} from "react-router-dom";
 import cloudy from './images/cloudy.png';
+import rainy from './images/rainy.png';
+import sunny from './images/sunny.png';
+import snowy from './images/snowy.png';
 function Monday() {
     let params = new URLSearchParams(useLocation().search);
     let date = params.get("date");
@@ -8,10 +11,14 @@ function Monday() {
     let current = params.get("current");
     let min = params.get("min");
     //substring check: https://stackoverflow.com/questions/1789945/how-to-check-whether-a-string-contains-a-substring-in-javascript
-    let image = '';
+    let image = sunny;
     if(description.substring('cloud')){
         image = cloudy;
-    }
+    } else if (description.substring('rain')){
+        image = rainy;
+    } else if (description.substring('snow')){
+        image = snowy;
+    } 
 return (
     <div>
         <p>Monday</p>
