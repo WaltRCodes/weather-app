fb18d002eb87bbf7c130b28132219dc266d43959
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-06 22:28:43 -0400

    added conditional

--- src/components/Monday.js
+++ src/components/Monday.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import {useLocation, useParams} from "react-router-dom";
-import Cloudy from '../images/cloudy.png';
+import cloudy from './images/cloudy.png';
 function Monday() {
     let params = new URLSearchParams(useLocation().search);
     let date = params.get("date");
     let description = params.get("description");
     let current = params.get("current");
     let min = params.get("min");
+    //substring check: https://stackoverflow.com/questions/1789945/how-to-check-whether-a-string-contains-a-substring-in-javascript
+    let image = '';
+    if(description.substring('cloud')){
+        image = cloudy;
+    }
 return (
     <div>
         <p>Monday</p>
         <p>{date}</p>
-        <p>{description}</p>
+        <p><img src={image}/></p>
         <p>{current}</p>
         <p>{min}</p>
     </div>
